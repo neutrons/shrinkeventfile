6f67ec82bcb8c4797823387c0b73afe2a09d154f
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-04-23 17:04:23 -0400

    Split out real and fake HDF5 test file + get_entries tests

--- tests/test_shrinkeventfile.py
+++ tests/test_shrinkeventfile.py
@@ -1,21 +1,32 @@
 import h5py
+import os
 import pytest
 import tempfile
 
+from tests import TEST_DATA_DIR
+
 import shrinkeventfile as shrink
-from shrinkeventfile.shrinkeventfile import product
+from shrinkeventfile.shrinkeventfile import get_entries, product
 
 INPUT_RUN="NOM_92628"
 INPUT_FILE="{}.nxs.h5".format(INPUT_RUN)
+FAKE_INPUT_FILE="{}_fake.nxs.h5".format(INPUT_RUN)
 
 @pytest.fixture
 def infile():
-    infile =  h5py.File(INPUT_FILE, 'w')
-    infile.attrs.create("HDF5_VERSION", b'1.8.5')
-    infile.close()
-
-    infile =  h5py.File(INPUT_FILE, 'r')
+    filename = os.path.join(TEST_DATA_DIR, INPUT_FILE)
+    infile =  h5py.File(filename, 'r')
     return infile
+
+@pytest.fixture
+def fake_infile():
+    filename = os.path.join(FAKE_INPUT_FILE)
+    fake_infile =  h5py.File(filename, 'w')
+    fake_infile.attrs.create("HDF5_VERSION", '1.8.5')
+    fake_infile.close()
+
+    fake_infile =  h5py.File(FAKE_INPUT_FILE, 'r')
+    return fake_infile
 
 @pytest.fixture
 def outfile():
@@ -30,20 +41,51 @@ def test_product():
     total = product(shape)
     assert total == 24
 
-def test_write_global_attrs(infile, outfile):
-    shrink.write_global_attrs(infile, outfile)
-    assert "HDF5_VERSION" in outfile.attrs
-    assert outfile.attrs.get("HDF5_VERSION") ==  b'1.8.5'
+def test_get_entries(infile):
+    result = get_entries(infile)
 
-def test_write_global_attrs_with_kwargs_to_skip_attribute(infile, outfile):
-    shrink.write_global_attrs(infile, outfile, HDF5_VERSION=None)
+    # NXcollection entry
+    assert "DASlogs" in result
+    assert result["DASlogs"] == "NXcollection"
+
+    # NXeven_data entry
+    assert "bank10_events" in result
+    assert result["bank10_events"] == "NXevent_data"
+
+    # SDS entry
+    assert "duration" in result
+    assert result["duration"] == "SDS"
+
+    # NXinstrument entry
+    assert "instrument" in result
+    assert result["instrument"] == "NXinstrument"
+
+    # NXmonitor entry
+    assert "monitor1" in result
+    assert result["monitor1"] == "NXmonitor"
+
+    # NXsample
+    assert "sample" in result
+    assert result["sample"] == "NXsample"
+
+    # NXuser
+    assert "user1" in result
+    assert result["user1"] == "NXuser"
+
+def test_write_global_attrs(fake_infile, outfile):
+    shrink.write_global_attrs(fake_infile, outfile)
+    assert "HDF5_VERSION" in outfile.attrs
+    assert outfile.attrs.get("HDF5_VERSION") ==  '1.8.5'
+
+def test_write_global_attrs_with_kwargs_to_skip_attribute(fake_infile, outfile):
+    shrink.write_global_attrs(fake_infile, outfile, HDF5_VERSION=None)
     assert "HDF5_VERSION" not in outfile.attrs
 
-def test_write_global_attrs_with_kwargs_to_overridte(infile, outfile):
-    shrink.write_global_attrs(infile, outfile, HDF5_VERSION=b'9.9.9')
+def test_write_global_attrs_with_kwargs_to_overridte(fake_infile, outfile):
+    shrink.write_global_attrs(fake_infile, outfile, HDF5_VERSION='9.9.9')
     assert "HDF5_VERSION" in outfile.attrs
-    assert outfile.attrs.get("HDF5_VERSION") ==  b'9.9.9'
+    assert outfile.attrs.get("HDF5_VERSION") ==  '9.9.9'
 
-def test_write_global_attrs_with_undefined_kwargs(infile, outfile):
+def test_write_global_attrs_with_undefined_kwargs(fake_infile, outfile):
     with pytest.raises(shrink.NoAttrInHDF5FileException):
-        shrink.write_global_attrs(infile, outfile, Foo="Bar")
+        shrink.write_global_attrs(fake_infile, outfile, Foo="Bar")
